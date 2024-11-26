3c6cfb5f9930f12f50d2c971b27fe9c0db6a3ace
Roman <romanalam8083@gmail.com>
2024-11-26 18:44:00 +0100

    added login page

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+    header("location:login.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
---
+++ login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="css/login.css" />
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <section class="h-100 gradient-form" style="background-color: #eee;">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-xl-10">
+            <div class="card rounded-3 text-black">
+            <div class="row g-0">
+                <div class="col-lg-6">
+                <div class="card-body p-md-5 mx-md-4">
+
+                    <div class="text-center">
+                        <h4 class="mt-1 mb-5 pb-1"><b>Programma Quiz</b></h4>
+                    </div>
+
+                    <form>
+                        <p>Login to your account</p>
+
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <input type="email" class="form-control" placeholder="Username" />
+                        </div>
+
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <input type="password" class="form-control" placeholder="Password" />
+                        </div>
+
+                        <div class="text-center pt-1 mb-5 pb-1">
+                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log in</button>
+                        </div>
+
+                    </form>
+
+                </div>
+                </div>
+                <div class="col-lg-6 d-flex align-items-center justify-content-center gradient-custom-2">
+                    <img src="resources/quiz.png" alt="quiz">
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"></script> 
+</body>
+</html>
