458f616c7e000d647a7a595444527a0311b245b9
MisticG29 <72145056+MisticG29@users.noreply.github.com>
2024-11-28 10:18:44 +0100

    Update programma_quiz.sql

--- programma_quiz.sql
+++ programma_quiz.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2024 at 09:18 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Nov 28, 2024 at 10:18 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,79 @@ SET time_zone = "+00:00";
 --
 -- Database: `programma_quiz`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classe`
+--
+
+CREATE TABLE `classe` (
+  `codice` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `domanda`
+--
+
+CREATE TABLE `domanda` (
+  `id` int(11) NOT NULL,
+  `testo` varchar(2000) NOT NULL,
+  `tipo` varchar(255) NOT NULL DEFAULT 'freetext',
+  `id_test` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `risposte`
+--
+
+CREATE TABLE `risposte` (
+  `id` int(11) NOT NULL,
+  `testo` varchar(2000) DEFAULT NULL,
+  `corretta` tinyint(1) NOT NULL,
+  `id_domanda` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `risultati`
+--
+
+CREATE TABLE `risultati` (
+  `id` int(11) NOT NULL,
+  `id_studente` int(11) NOT NULL,
+  `id_test` int(11) NOT NULL,
+  `punteggio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessione_test`
+--
+
+CREATE TABLE `sessione_test` (
+  `id` int(11) NOT NULL,
+  `id_test` int(11) NOT NULL,
+  `codice_classe` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL,
+  `titolo` varchar(255) NOT NULL,
+  `descrizione` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -48,6 +121,48 @@ INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `e
 --
 
 --
+-- Indexes for table `classe`
+--
+ALTER TABLE `classe`
+  ADD PRIMARY KEY (`codice`);
+
+--
+-- Indexes for table `domanda`
+--
+ALTER TABLE `domanda`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_test` (`id_test`);
+
+--
+-- Indexes for table `risposte`
+--
+ALTER TABLE `risposte`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_domanda` (`id_domanda`);
+
+--
+-- Indexes for table `risultati`
+--
+ALTER TABLE `risultati`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_studente` (`id_studente`),
+  ADD KEY `id_test` (`id_test`);
+
+--
+-- Indexes for table `sessione_test`
+--
+ALTER TABLE `sessione_test`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_test` (`id_test`),
+  ADD KEY `codice_classe` (`codice_classe`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -59,10 +174,64 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `domanda`
+--
+ALTER TABLE `domanda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `risultati`
+--
+ALTER TABLE `risultati`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sessione_test`
+--
+ALTER TABLE `sessione_test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `domanda`
+--
+ALTER TABLE `domanda`
+  ADD CONSTRAINT `domanda_ibfk_1` FOREIGN KEY (`id_test`) REFERENCES `test` (`id`);
+
+--
+-- Constraints for table `risposte`
+--
+ALTER TABLE `risposte`
+  ADD CONSTRAINT `risposte_ibfk_1` FOREIGN KEY (`id_domanda`) REFERENCES `domanda` (`id`);
+
+--
+-- Constraints for table `risultati`
+--
+ALTER TABLE `risultati`
+  ADD CONSTRAINT `risultati_ibfk_1` FOREIGN KEY (`id_studente`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `risultati_ibfk_2` FOREIGN KEY (`id_test`) REFERENCES `test` (`id`);
+
+--
+-- Constraints for table `sessione_test`
+--
+ALTER TABLE `sessione_test`
+  ADD CONSTRAINT `sessione_test_ibfk_1` FOREIGN KEY (`id_test`) REFERENCES `test` (`id`),
+  ADD CONSTRAINT `sessione_test_ibfk_2` FOREIGN KEY (`codice_classe`) REFERENCES `classe` (`codice`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
