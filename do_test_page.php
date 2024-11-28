8c29931b5bd6f1abc5c262feb9de5181d30d8aa5
Roman <romanalam8083@gmail.com>
2024-11-28 10:43:21 +0100

    Update do_test_page.php

--- do_test_page.php
+++ do_test_page.php
@@ -27,7 +27,7 @@
 
             <h3 class="fw-bold">Welcome <?php echo $_SESSION["username"]; ?>!</h3>
 
-            <h4 class="fw-bold ms-auto" >Do test</h4>
+            <h4 class="fw-bold ms-auto text-center" >Do test</h4>
 
             <form class="d-flex" method="POST" action="php/do_logout.php">
                 <input class="btn btn-primary ms-auto" type="submit" id="logout" name="logout" value="logout">
@@ -35,10 +35,28 @@
         </div>
     </nav>
 
+    <div class="mt-3">
+
+        <h2 class="fw-bold text-center my-5">Nome Test</h2>
+        <?php 
+
+
+
+        ?>
+
+        <div class="d-flex justify-content-center">
+            <div class="card" style="width: 75vw;">
+            <div class="card-body">
+                <h5 class="card-title">Card Title</h5>
+                <p class="card-text">This is a simple card example with some text content to describe the purpose of the card.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> 
 </body>
 </html>
