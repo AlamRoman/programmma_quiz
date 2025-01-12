c930e68157c0cf4a5247378a633600a845f71b15
Roman <romanalam8083@gmail.com>
2025-01-12 21:52:49 +0100

    Update sessione_test.php

--- sessione_test.php
+++ sessione_test.php
@@ -9,6 +9,37 @@
         go_to("login_page.php");
     }
 
+    function aggiornaStatoSessione($id_sessione, $data_inizio, $data_fine) {
+        global $conn;
+        $current_date = date("Y-m-d H:i:s");
+    
+        $stato = "";
+        
+        if ($current_date < $data_inizio) {
+            $stato = 'programmato';
+        } elseif ($current_date >= $data_inizio && $current_date <= $data_fine) {
+            $stato = 'in corso';
+        } else {
+            $stato = 'completato';
+        }
+    
+        $sql_update = "UPDATE sessione_test SET stato = ? WHERE id = ?";
+    
+        if ($stmt = $conn->prepare($sql_update)) {
+            $stmt->bind_param("si", $stato, $id_sessione);
+            if ($stmt->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    
+
+
+    //prendi le sessioni
     $sql = "
         SELECT 
         s.id, 
@@ -31,6 +62,8 @@
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $result = $stmt->get_result();
+
+
 
 ?>
 <!DOCTYPE html>
@@ -80,6 +113,36 @@
             unset($_SESSION["sessione_test_creato"]);
         }
 
+        if (isset($_GET["deleted"])) {
+            if ($_GET["deleted"] == 1) {
+                echo '<div class="container mt-5"><div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <i class="bi bi-check-circle-fill"></i> Sessione test eliminato con successo!
+                        <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+                    </div></div>';
+            } elseif ($_GET["deleted"] == 0) {
+                echo '<div class="container mt-5"><div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <i class="bi bi-x-circle-fill"></i> Errore durante l\'eliminazione della sessione test.
+                        <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+                    </div></div>';
+            }
+        }
+
+        if(isset($_SESSION["testModificato"])){
+
+            if($_SESSION["testModificato"]==1){
+                echo '<div class="container mt-5"><div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <i class="bi bi-check-circle-fill"></i> Test modificato correttamente !
+                    <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+                </div></div>';
+            }else{
+                echo '<div class="container mt-5"><div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <i class="bi bi-x-circle-fill"></i> Errore durante la modifica del test
+                        <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+                    </div></div>';
+            }
+            
+            unset($_SESSION["testModificato"]);
+        }
     ?>
 
     <div class="container mt-5">
@@ -94,14 +157,29 @@
             <?php
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
+
+                        aggiornaStatoSessione($row["id"], $row["data_inizio"], $row["data_fine"]);
+
                         echo '<li class="list-group-item list-group-item-action d-flex justify-content-between border">';
                         echo '<div>';
                         echo '<h6>' . htmlspecialchars($row['nome']) . '</h6>';
                         echo '<p>' . htmlspecialchars($row['nome_classe']) . ' - '. htmlspecialchars($row['titolo_test'])  . '</p>';
                         echo '</div>';
+
+                        $bg_color = "";
+
+                        if ($row['stato'] == "programmato") {
+                            $bg_color = 'btn-secondary';
+                        } elseif ($row['stato'] == "in corso") {
+                            $bg_color = 'btn-warning';
+                        } else {
+                            $bg_color = 'btn-success';
+                        }
+
                         echo '<div class="my-auto d-flex gap-3">';
+                        echo '<button style="width: 200px" class="btn me-5 ' . $bg_color . ' btn-sm">' . $row['stato'] . '</button>';
                         echo '<a href="#" class="btn btn-danger" onclick="return confirmDelete(' . $row['id'] . ')"><i class="bi bi-trash"></i></a>';
-                        echo '<a class="btn btn-secondary" href="modificaTest.php?id_test='.$row["id"].'"><i class="bi bi-pencil-square"></i></a>';
+                        echo '<a class="btn btn-secondary" href="modificaSessioneTest.php?id='.$row["id"].'"><i class="bi bi-pencil-square"></i></a>';
                         echo '</div>';
                         echo '</li>';
                     }
@@ -113,6 +191,16 @@
             </ul>
         </div>
     </div>
+
+    <script>
+        function confirmDelete(id) {
+            var confirmDelete = confirm("Sei sicuro di voler eliminare questa sessione test?");
+            if (confirmDelete) {
+                window.location.href = "php/deleteTestSession.php?id=" + id;
+            }
+            return false;
+        }
+    </script>
     
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
